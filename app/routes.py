fbe69de9b77f658935ba5bd53231cc67974c2a4a
WINDOWS-DFTF1DT\Dell <croug327@gmail.com>
2016-11-06 18:08:20 -0500

    get all genres route

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from resources.search_genre import Search_Genre
 from resources.search_castcrew import Search_Castcrew
 from resources.movie_id import Movie_Id
 from resources.person_id import Person_Id
+from resources.get_genre_names import Get_Genre_Names
 
 API_PREFIX = '/api/v1'
 SEARCH_SUFFIX = '/search_title'
@@ -10,6 +11,7 @@ GENRE_SUFFIX = '/search_genre'
 CAST_CREW_SUFFIX = '/search_castcrew'
 MOVIE_ID_SUFFIX = '/movie'
 PERSON_ID_SUFFIX = '/person'
+Get_Genre_Names_SUFFIX = '/get_all_genres'
 
 def init_routes(api):
     api.add_resource(Search_Title, API_PREFIX + SEARCH_SUFFIX)
@@ -17,3 +19,4 @@ def init_routes(api):
     api.add_resource(Search_Castcrew, API_PREFIX + CAST_CREW_SUFFIX)
     api.add_resource(Movie_Id, API_PREFIX + MOVIE_ID_SUFFIX)
     api.add_resource(Person_Id, API_PREFIX + PERSON_ID_SUFFIX)
+    api.add_resource(Get_Genre_Names, API_PREFIX + Get_Genre_Names_SUFFIX)
