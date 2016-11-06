55ce640d148b4386323f25180b0606f13f2d131d
WilliamWickerson <willie7296@gmail.com>
2016-11-06 17:20:01 -0500

    Made cast/crew search more modular

--- queries/movies_by_castcrew.py
+++ queries/movies_by_castcrew.py
@@ -5,42 +5,58 @@ from config.config import *
 ORACLE_CONN_STRING = sql_login
 
 SQL_STATEMENT = (
-                "WITH likeNameMovie1 AS "
-                    "(SELECT * FROM nameMovie nm "
-                     "WHERE nm.name like '{}'), "
-                     "likeNameMovie2 AS "
-                    "(SELECT * FROM nameMovie nm "
-                     "WHERE nm.name like '{}') "
-                "SELECT DISTINCT lnm1.movie_id, lnm1.title, lnm1.budget, lnm1.overview, lnm1.popularity, lnm1.rating_average, lnm1.rating_count, lnm1.poster_path, lnm1.release_date, lnm1. revenue, lnm1.run_time "
-                    "FROM likeNameMovie1 lnm1, likeNameMovie2 lnm2 "
-                    "WHERE lnm1.movie_id = lnm2.movie_id "
-                    "AND lnm1.name <> lnm2.name "
-                    "order by lnm1.popularity desc "
+                "WITH {} "
+                "SELECT DISTINCT lnm0.movie_id, lnm0.title, lnm0.budget, lnm0.overview, lnm0.popularity, lnm0.rating_average, lnm0.rating_count, lnm0.poster_path, lnm0.release_date, lnm0. revenue, lnm0.run_time "
+                    "FROM {} "
+                    "{} "
+                    "order by lnm0.popularity desc "
                 )
+
+SQL_STATEMENT_DEFAULT = "SELECT * FROM Movies"
 
 def rows_to_dict_list(cursor):
     columns = [i[0] for i in cursor.description]
     columns = [column.lower() for column in columns]
     return [dict(zip(columns, row)) for row in cursor]
 
-def get_movie_by_castcrew(name1, name2, limit):
-    if '%' not in name1:
-        name1 = '%' + name1 + '%'
-
-    if '%' not in name2:
-        name2 = '%' + name2 + '%'
-
+def get_movie_by_castcrew(name):
     con = cx_Oracle.connect(ORACLE_CONN_STRING)
     cursor = con.cursor()
+    ret = {}
 
-    cursor.execute(SQL_STATEMENT.format(name1, name2).replace('\n', ''))
+    if not name:
+        cursor.execute(SQL_STATEMENT_DEFAULT)
+    elif not name[0]:
+        cursor.execute(SQL_STATEMENT_DEFAULT)
+    else:
+        table = (
+            "likeNameMovie{} AS "
+            "(SELECT * FROM nameMovie nm "
+            "WHERE nm.name like '%{}%') "
+            )
+        tables = table.format(0,name[0])
+        fromTable = "likeNameMovie{} lnm{}"
+        fromTables = fromTable.format(0,0)
+        notEqual = "lnm0.movie_id = lnm{}.movie_id "
+        if (len(name) == 1):
+            notEquals = ""
+        else:
+            notEquals = "WHERE "
+        for i in range(1,len(name)):
+            if name[i]:
+                tables += ", "
+                tables += table.format(i,name[i])
+                fromTables += ", "
+                fromTables += fromTable.format(i,i)
+                if (i > 1):
+                    notEquals += "AND "
+                notEquals += notEqual.format(i,i)
+
+        cursor.execute(SQL_STATEMENT.format(tables, fromTables, notEquals).replace('\n', ''))
 
     ret = rows_to_dict_list(cursor)
 
-    if limit == None:
-        limit = len(ret)
-
     con.close()
-    return ret[0:limit]
+    return ret
 
 
--- resources/search_castcrew.py
+++ resources/search_castcrew.py
@@ -5,11 +5,9 @@ from queries.movies_by_castcrew import get_movie_by_castcrew
 class Search_Castcrew(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('name1', default='%')
-        parser.add_argument('name2', default='%')
-        parser.add_argument('limit', type=int)
+        parser.add_argument('name', action='append', default=[])
         args = parser.parse_args()
 
-        movies = get_movie_by_castcrew(args['name1'], args['name2'], args['limit'])
+        movies = get_movie_by_castcrew(args['name'])
 
         return movies
