8338d02d002787a63612ac1591327d78b3ffc452
Chase Farmer <chasefarmer2808@ufl.edu>
2016-11-23 22:30:57 -0500

    title search limit

--- resources/search_title.py
+++ resources/search_title.py
@@ -6,8 +6,9 @@ class Search_Title(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('title', default="'%'")
+        parser.add_argument('limit', type=int, default=0)
         args = parser.parse_args()
 
-        movies = get_movie_by_title(args['title'])
+        movies = get_movie_by_title(args['title'], args['limit'])
 
         return movies
