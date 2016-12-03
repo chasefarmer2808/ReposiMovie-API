1c314bea9f06e93609bc6631cb5e43102335697c
WilliamWickerson <willie7296@gmail.com>
2016-12-03 16:15:09 -0500

    made get all companies better and added preliminary adv_search

--- queries/get_all_companies.py
+++ queries/get_all_companies.py
@@ -4,21 +4,18 @@ from config.config import *
 
 ORACLE_CONN_STRING = sql_login
 
-SQL_STATEMENT = ("select distinct company.company_id, company.name from company, produced_by "
-                    "where company.company_id = produced_by.company_id "
+SQL_STATEMENT = ("select distinct name from company "
                     "order by company.name")
 
-def rows_to_dict_list(cursor):
-    columns = [i[0] for i in cursor.description]
-    columns = [column.lower() for column in columns]
-    return [dict(zip(columns, row)) for row in cursor]
+def rows_to_array(cursor):
+    return [row[0] for row in cursor]
 
 def get_all_companies():
     con = cx_Oracle.connect(ORACLE_CONN_STRING)
     cursor = con.cursor()
     cursor.execute(SQL_STATEMENT)
 
-    ret = rows_to_dict_list(cursor)
+    ret = rows_to_array(cursor)
 
     con.close()
     return ret
--- resources/adv_search.py
+++ resources/adv_search.py
@@ -1,6 +1,7 @@
 import datetime
 
 from flask_restful import Resource, reqparse, abort
+from queries.adv_search import get_movies_advanced
 
 DATE_FORMAT = '%m-%d-%Y'
 
@@ -10,44 +11,20 @@ class Adv_Search(Resource):
         parser.add_argument('title', default="%")
         parser.add_argument('startDate', required=True)
         parser.add_argument('endDate', required=True)
-        parser.add_argument('avgRating', default='0>')
-        parser.add_argument('ratingCount', default='0>')
-        parser.add_argument('revenue', default='0>')
-        parser.add_argument('budget', default='0>')
-        parser.add_argument('runtime', default='0>')
+        parser.add_argument('avgRating', default='0 >')
+        parser.add_argument('ratingCount', default='0 >')
+        parser.add_argument('revenue', default='0 >')
+        parser.add_argument('budget', default='0 >')
+        parser.add_argument('runtime', default='0 >')
         parser.add_argument('genres', action='append', default=[])
         parser.add_argument('companies', action='append', default=[])
         parser.add_argument('people', action='append', default=[])
         parser.add_argument('limit', type=int)
         args = parser.parse_args()
 
-        args['startDate'] = datetime.datetime.strptime(args['startDate'], DATE_FORMAT)
-        args['endDate'] = datetime.datetime.strptime(args['endDate'], DATE_FORMAT)
+        convert = datetime.datetime.strptime
 
-        if args['startDate'] > args['endDate']:
-            abort(400, message='Start date must be before End date')
+        if convert(args['startDate'], DATE_FORMAT) > convert(args['endDate'], DATE_FORMAT):
+            return []
 
-        args['avgRating'] = {
-            'value': args['avgRating'][0:len(args['avgRating'])-1],
-            'equality': args['avgRating'][len(args['avgRating'])-1]
-        }
-        args['ratingCount'] = {
-            'value': args['ratingCount'][0:len(args['ratingCount']) - 1],
-            'equality': args['ratingCount'][len(args['ratingCount']) - 1]
-        }
-        args['revenue'] = {
-            'value': args['revenue'][0:len(args['revenue']) - 1],
-            'equality': args['revenue'][len(args['revenue']) - 1]
-        }
-        args['budget'] = {
-            'value': args['budget'][0:len(args['budget']) - 1],
-            'equality': args['budget'][len(args['budget']) - 1]
-        }
-        args['runtime'] = {
-            'value': args['runtime'][0:len(args['runtime']) - 1],
-            'equality': args['runtime'][len(args['runtime']) - 1]
-        }
-
-        print(args)
-
-        return
+        return args,
